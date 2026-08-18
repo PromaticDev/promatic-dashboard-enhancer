a87703feab5fe91e973ca71585668df7caed5b7a
OrcaCL <124632805+OrcaCl@users.noreply.github.com>
2026-08-18 13:06:43 -0400

    sync: publish plugin 2026-08-18_1306

--- Module.js
+++ Module.js
@@ -1,7 +1,7 @@
 Ext.define('Store.promatic_dashboard_enhancer.Module', {
     extend: 'Ext.Component',
     extensionName: 'promatic_dashboard_enhancer',
-    moduleBuild: '2026-08-18-03',
+    moduleBuild: '2026-08-18-04',
 
     initModule: function () {
         console.log('[promatic_dashboard_enhancer] BUILD ' + this.moduleBuild + ' — initModule: inicio');
